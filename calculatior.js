6c78b46dc01feee8c24fd7e06920b0a43a192e77
ZoeChelimo <zoechelimo6@gmail.com>
2024-03-13 10:04:36 +0300

    first commit

---
+++ calculatior.js
@@ -0,0 +1,11 @@
+
+function appendToDisplay(input){
+  Display.value+=input
+}
+function clearDisplay(){
+    Display.value=""
+
+}
+function calculateResult(){
+ Display.value=eval(Display.value)
+}
